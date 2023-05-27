3ca99eb98beacb81e2e324f4a6ec8ee5ce16b1a5
to-sawaguchi <123784874+to-sawaguchi@users.noreply.github.com>
2023-05-27 11:47:05 +0900

    Update camera.py

--- src/camera.py
+++ src/camera.py
@@ -53,45 +53,44 @@ class VideoCamera(object):
         if success and image is not None:
 
             if image is not None:  # 画像が空でないことをチェック
-                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            image.flags.writeable = False
-            results = self.pose.process(image)
-            image.flags.writeable = True
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            try:
-                landmarks = results.pose_landmarks.landmark
-                shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-                elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-                wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
-                angle = self.calculate_angle(shoulder, elbow, wrist)
-                if angle > 160:
-                    self.stage = "down"
-                if angle < 30 and self.stage =='down':
-                    self.stage = "up"
-                    self.counter += 1
-                    self.timer = time.time()  
-            except:
-                pass
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                image.flags.writeable = False
+                results = self.pose.process(image)
+                image.flags.writeable = True
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                try:
+                    landmarks = results.pose_landmarks.landmark
+                    shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                    elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+                    wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+                    angle = self.calculate_angle(shoulder, elbow, wrist)
+                    if angle > 160:
+                        self.stage = "down"
+                    if angle < 30 and self.stage =='down':
+                        self.stage = "up"
+                        self.counter += 1
+                        self.timer = time.time()  
+                except:
+                    pass
 
-            if self.timer is not None and time.time() - self.timer > 6:
-                self.final_count = self.counter
-                self.counter = 0
-                self.timer = None
+                if self.timer is not None and time.time() - self.timer > 6:
+                    self.final_count = self.counter
+                    self.counter = 0
+                    self.timer = None
 
-            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                    mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
-                                    mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2))
+                mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                        mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                        mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2))
 
-            cv2.putText(image, 'REPS: '+str(self.counter), 
-                        (10,60), 
-                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
-            cv2.putText(image, 'STAGE: '+str(self.stage), 
-                        (10,120), 
-                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
-            ret, jpeg = cv2.imencode('.jpg', image)
-            return jpeg.tobytes()
+                cv2.putText(image, 'REPS: '+str(self.counter), 
+                            (10,60), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+                cv2.putText(image, 'STAGE: '+str(self.stage), 
+                            (10,120), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+                ret, jpeg = cv2.imencode('.jpg', image)
+                return jpeg.tobytes()
 
         else:
             print("Failed to capture frame from camera.")
